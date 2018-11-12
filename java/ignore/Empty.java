9b786c264d8a80239aaf2b703a34c8205833d0f2
Stefan Reichhart <stefan.reichhart@zuehlke.com>
2018-11-12 22:05:07 +0100

    index slide

---
+++ java/ignore/Empty.java
@@ -0,0 +1,9 @@
+public class Empty {
+    public static void main(String[] args) {
+
+        // "C:\Program Files\Java\jdk-12\bin\javac.exe" --enable-preview --release 12 Empty.java
+        // "C:\Program Files\Java\jdk-12\bin\java.exe" --enable-preview Empty
+        System.out.print("--- Hello World ---");
+
+    }
+}
