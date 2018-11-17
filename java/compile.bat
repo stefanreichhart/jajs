4acc3aaea8c0545b482c8b29b3ee725f1a4b3994
stefanreichhart <stefan.reichhart@zuehlke.com>
2018-11-17 15:50:21 +0100

    simple jigsaw demo

--- java/compile.bat
+++ java/compile.bat
@@ -5,17 +5,17 @@ mkdir mods
 
 echo "Compiling sources and modules ..."
 
-# destination: out
-# lookup of other modules: src
-# module sources for compilation: src
-# sources for compilation: *.java
+REM destination: out
+REM lookup of other modules: src
+REM module sources for compilation: src
+REM sources for compilation: *.java
 "C:\Program Files\Java\jdk-11.0.1\bin\javac.exe" -d out --module-path src --module-source-path src src\modA\a1\A1.java src\modB\b1\B1.java src\modC\c1\C1.java
 if errorlevel 1 goto :endofscript
 
 echo "Packaging modules ..."
 
-# create jar file: mods\mod*.jar
-# create from module: out/mod*
+REM create jar file: mods\mod*.jar
+REM create from module: out/mod*
 "C:\Program Files\Java\jdk-11.0.1\bin\jar.exe" --create --file=mods\modA.jar -C out/modA .
 if errorlevel 1 goto :endofscript
 
@@ -27,8 +27,8 @@ if errorlevel 1 goto :endofscript
 
 echo "Executing Modules/Main ..."
 
-# run class: modA/a1/A1
-# module lookup path: mods
+REM run class: modA/a1/A1
+REM module lookup path: mods
 "C:\Program Files\Java\jdk-11.0.1\bin\java.exe" --module-path mods -m modA/a1.A1
 if errorlevel 1 goto :endofscript
 
