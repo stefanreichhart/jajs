99c29a43ed8b2746b55f4b6c14e8f00751312aea
Stefan Reichhart <stefan.reichhart@zuehlke.com>
2019-01-05 14:33:22 +0100

    reorder pages

--- java/compile.bat
+++ java/compile.bat
@@ -1,9 +1,14 @@
 echo OFF
 
+echo:
+echo "0) Cleaning up ..."
+
 mkdir out
 mkdir mods
+rm mods/modA.* mods/modB.* mods/modC.*
 
-echo "Compiling sources and modules ..."
+echo:
+echo "1) Compiling sources and modules ..."
 
 REM destination: out
 REM lookup of other modules: src
@@ -12,20 +17,25 @@ REM sources for compilation: *.java
 "C:\Program Files\Java\jdk-11.0.1\bin\javac.exe" -d out --module-path src --module-source-path src src\modA\a1\A1.java src\modB\b1\B1.java src\modC\c1\C1.java
 if errorlevel 1 goto :endofscript
 
-echo "Packaging modules ..."
+echo:
+echo "2) Packaging modules ..."
 
 REM create jar file: mods\mod*.jar
 REM create from module: out/mod*
 "C:\Program Files\Java\jdk-11.0.1\bin\jar.exe" --create --file=mods\modA.jar -C out/modA .
+REM "C:\Program Files\Java\jdk-11.0.1\bin\jmod.exe" create --module-path mods --class-path out\modA mods\modA.jmod
 if errorlevel 1 goto :endofscript
 
 "C:\Program Files\Java\jdk-11.0.1\bin\jar.exe" --create --file=mods\modB.jar -C out/modB .
+REM "C:\Program Files\Java\jdk-11.0.1\bin\jmod.exe" create --module-path mods --class-path out\modB mods\modB.jmod
 if errorlevel 1 goto :endofscript
 
 "C:\Program Files\Java\jdk-11.0.1\bin\jar.exe" --create --file=mods\modC.jar -C out/modC .
+REM "C:\Program Files\Java\jdk-11.0.1\bin\jmod.exe" create --module-path mods --class-path out\modC mods\modC.jmod
 if errorlevel 1 goto :endofscript
 
-echo "Executing Modules/Main ..."
+echo:
+echo "3) Executing Modules/Main ..."
 
 REM run class: modA/a1/A1
 REM module lookup path: mods
@@ -39,6 +49,6 @@ if errorlevel 1 goto :endofscript
 if errorlevel 1 goto :endofscript
 
 :endofscript
-echo ""
+echo:
 
 echo ON
